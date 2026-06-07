1c5b7a3810c08dd60fb7132f4bbb72f9d710087b
Ludovit Scholtz <ludovit.scholtz@aaaauto.cz>
2026-06-07 10:50:35 +0200

    feat: update TypeScript configuration and Vite setup for improved module resolution and aliasing

--- projects/algo-safe-frontend/vite.config.ts
+++ projects/algo-safe-frontend/vite.config.ts
@@ -1,4 +1,5 @@
 import react from '@vitejs/plugin-react'
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
@@ -13,6 +14,21 @@ export default defineConfig({
     }),
   ],
   resolve: {
+    alias: {
+      'algo-safe': resolve(__dirname, '../algo-safe-contracts/dist/index.mjs'),
+      'vite-plugin-node-polyfills/shims/buffer': resolve(
+        __dirname,
+        'node_modules/vite-plugin-node-polyfills/shims/buffer/dist/index.js',
+      ),
+      'vite-plugin-node-polyfills/shims/process': resolve(
+        __dirname,
+        'node_modules/vite-plugin-node-polyfills/shims/process/dist/index.js',
+      ),
+      'vite-plugin-node-polyfills/shims/global': resolve(
+        __dirname,
+        'node_modules/vite-plugin-node-polyfills/shims/global/dist/index.js',
+      ),
+    },
     dedupe: ['algosdk'],
   },
   optimizeDeps: {
